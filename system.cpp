86d5153521f3bdac09e8a4ae7d895b4db07e6294
russellkoons <rkoons@gmail.com>
2019-06-28 16:03:36 -0400

    added some placeholder text

---
+++ system.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "Hello!" << endl;
+  cout << "This is where my C++ game will be." << endl;
+  cout << "Still trying to figure out what I want it to be..." << endl;
+  cout << "Check back later!" << endl;
+}
