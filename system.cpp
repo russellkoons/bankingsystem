6ec8f1ed840e88de0e26b5ce2b5ed3571a28a8de
russellkoons <rkoons@gmail.com>
2019-07-10 14:40:32 -0400

    Game ideas

--- system.cpp
+++ system.cpp
@@ -2,11 +2,22 @@
 
 using namespace std;
 
+// Game ideas:
+
+  // Dystopic future (present?) where the world is even worse off from corportatism and global warming
+    // Maybe too much like Oryx and Crake?
+
+  // Make an actual banking system like the initial assignment suggested but add a bunch of dumb jokes
+
+  // Fantasy based CYOA
+    // Maybe base it on the outline you made up two years ago
+
+  // Western CYOA based on country songs
+
 int main()
 {
   cout << "Hello!" << endl;
   cout << "This is where my C++ game will be." << endl;
   cout << "Still trying to figure out what I want it to be..." << endl;
   cout << "Check back later!" << endl;
-  cout << "Don't let the bastards win!" << endl;
 }
